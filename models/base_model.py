94a2b7f4e0464ba069f6b0f5fb19d2b4be8a82d2
Falendrac <leger_franck@hotmail.fr>
2022-02-28 14:31:56 +0100

    Update the init method to handle kwargs.

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 Create the Base Model class
 """
 
+
 import uuid
 from datetime import datetime
 
@@ -12,14 +13,22 @@ class BaseModel:
     The Base Model of the AirBnB
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Init the Base Model class
         """
-        timming = datetime.now()
-        self.id = str(uuid.uuid4())
-        self.created_at = timming
-        self.updated_at = timming
+        if len(kwargs) == 0:
+            timming = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = timming
+            self.updated_at = timming
+        else:
+            for key, val in kwargs.items():
+                if key != '__class__':
+                    if key != 'created_at' and key != 'updated_at':
+                        setattr(self, key, val)
+                    else:
+                        setattr(self, key, datetime.fromisoformat(val))
 
     def __str__(self):
         """
