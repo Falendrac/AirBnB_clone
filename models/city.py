1585dfdfec6583e60b998dc33b37b9dad6bb2cc2
Falendrac <leger_franck@hotmail.fr>
2022-03-01 10:52:00 +0100

    Add classes of task 9.

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module for City class
+"""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """
+    The class City inherits of BaseModel
+    """
+    state_id = ""
+    name = ""
